a7d9b72c6bab7a33a7654a3d33cea12c0b8d5f34
Afiq Ilyasa Akmal <afiqilyasaakmal01@gmail.com>
2024-03-10 11:25:50 +0700

    add popup

--- src/app/artikel/create/page.js
+++ src/app/artikel/create/page.js
@@ -48,48 +48,50 @@ const CreateArticle = () => {
   return (
     <div>
       <Navbar />
-      <div class="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md max-w-screen-lg">
-        <h1 class="text-2xl font-semibold mb-4">Buat Berita</h1>
+      <div className="container mx-auto mt-8 p-8 bg-white rounded-lg shadow-md max-w-screen-lg">
+        <h1 className="text-2xl font-semibold mb-4">Buat Berita</h1>
         <form onSubmit={handleSubmit}>
-        <div>
-          <div class="mb-4">
-                <label htmlFor="judulArtikel" class="block text-gray-700 font-bold mb-2">Judul Berita</label>
-                <input type="text" id="judulArtikel" value={judulArtikel} onChange={(e) => setJudulArtikel(e.target.value)} name="judul" class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:border-blue-500"/>
+          <div>
+            <div className="mb-4">
+              <label htmlFor="judulArtikel" className="block text-gray-700 font-bold mb-2">Judul Berita</label>
+              <input type="text" id="judulArtikel" value={judulArtikel} onChange={(e) => setJudulArtikel(e.target.value)} name="judul" className="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:border-blue-500" />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="isiArtikel" className="block text-gray-700 font-bold mb-2">Isi Artikel:</label>
+              <textarea name="isi" rows="5" className="border border-gray-300 rounded-md py-2 px-4 w-full resize-none focus:outline-none focus:border-blue-500" id="isiArtikel" value={isiArtikel} onChange={(e) => setIsiArtikel(e.target.value)} />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="gambar" className="block text-gray-700 font-bold mb-2">Gambar:</label>
+              <input name="gambar" className="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:border-blue-500" type="file" id="gambar" onChange={(e) => setGambar(e.target.files[0])} />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="kategori" className="block text-gray-700 font-bold mb-2">Kategori:</label>
+              <input className="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:border-blue-500" type="text" id="kategori" value={kategori} onChange={(e) => setKategori(e.target.value)} />
+            </div>
+            <div className="flex justify-center">
+              <button type="submit" className="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Submit</button>
+            </div>
           </div>
-
-          <div class="mb-4">
-          <label htmlFor="isiArtikel" class="block text-gray-700 font-bold mb-2">Isi Artikel:</label>
-          <textarea name="isi" rows="5" class="border border-gray-300 rounded-md py-2 px-4 w-full resize-none focus:outline-none focus:border-blue-500" id="isiArtikel" value={isiArtikel} onChange={(e) => setIsiArtikel(e.target.value)} />
-        </div>
-
-        <div class="mb-4">
-          <label htmlFor="gambar" class="block text-gray-700 font-bold mb-2">Gambar:</label>
-          <input name="gambar" class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:border-blue-500" type="file" id="gambar" onChange={(e) => setGambar(e.target.files[0])} />
-        </div>
-        
-        <div class="mb-4">
-          <label htmlFor="kategori" class="block text-gray-700 font-bold mb-2">Kategori:</label>
-          <input class="border border-gray-300 rounded-md py-2 px-4 w-full focus:outline-none focus:border-blue-500" type="text" id="kategori" value={kategori} onChange={(e) => setKategori(e.target.value)} />
-        </div>
-        <div class="flex justify-center">
-          <button type="submit" class="bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Submit</button>
-        </div>
-        
-        </div>
-      </form>
+        </form>
       </div>
-      
-      
+
       {isSuccess && (
-        <div className="popup success">
-          <p>Artikel berhasil dibuat!</p>
-          <button onClick={handleClosePopup}>Tutup</button>
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg shadow-md absolute">
+            <p className="text-green-600 font-semibold">Artikel berhasil dibuat!</p>
+            <button onClick={handleClosePopup} className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Tutup</button>
+          </div>
         </div>
       )}
       {isError && (
-        <div className="popup error">
-          <p>Gagal membuat artikel!</p>
-          <button onClick={handleClosePopup}>Tutup</button>
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+          <div className="bg-white p-8 rounded-lg shadow-md absolute">
+            <p className="text-red-600 font-semibold">Gagal membuat artikel!</p>
+            <button onClick={handleClosePopup} className="mt-4 bg-blue-500 text-white py-2 px-4 rounded-md hover:bg-blue-600 transition duration-300">Tutup</button>
+          </div>
         </div>
       )}
       <Footer />
