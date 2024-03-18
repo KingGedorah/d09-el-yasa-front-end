4f95f2c6f0fc131f7b2ac2b0e5eb1b3e49bca8ed
Afiq Ilyasa Akmal <afiqilyasaakmal01@gmail.com>
2024-03-18 12:38:27 +0700

    create sticky footer

--- src/app/components/footer.js
+++ src/app/components/footer.js
@@ -2,10 +2,10 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <footer class="bg-blue-500 text-white text-center py-4">
-        <div class="container mx-auto">
-            <p>&copy; 2024 MyJISc</p>
-        </div>
+    <footer style={{ position: 'fixed', left: 0, bottom: 0, width: '100%', backgroundColor: '#007bff', color: 'white', textAlign: 'center', padding: '1rem' }}>
+      <div className="container mx-auto">
+        <p>&copy; 2024 MyJISc</p>
+      </div>
     </footer>
   );
 };
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -6,11 +6,12 @@ const Navbar = () => {
         <div class="flex items-center text-white">
             <img src="https://www.jakartaislamicschool.com/wp-content/uploads/2023/11/logo-web-sekolah-islam-terbaik-jakarta-timur-jisc.png" alt="Logo" class="h-8 mr-4"/>
             <ul class="flex space-x-4">
-                <li><a href="#" class="text-white">Beranda</a></li>
+                <li><a href="/" class="text-white">Beranda</a></li>
                 <li><a href="#" class="text-white">Akademik</a></li>
                 <li><a href="#" class="text-white">Inventaris</a></li>
-				<li><a href="#" class="text-white">Manajemen Konten</a></li>
-				<li><a href="#" class="text-white">Tentang Kami</a></li>
+                <li><a href="/artikel" class="text-white">Artikel</a></li>
+                <li><a href="/berita" class="text-white">Berita</a></li>
+                <li><a href="#" class="text-white">Tentang Kami</a></li>
             </ul>
         </div>
         <div class="flex items-center">
